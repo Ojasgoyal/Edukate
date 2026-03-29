48fe6f6f5a5018d367641aacc0f493b5b19fc076
Ojas Goyal <ojasgoyal05@gmail.com>
2026-03-29 20:20:35 +0530

    Hero Section

---
+++ frontend/src/components/Landing/Hero.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function () {
+  return (
+    <div className='flex justify-center sm:justify-between mt-24 mx-auto p-10'>
+        <div className='flex flex-col items-center sm:items-start'>
+            <div className='bg-amber-100 w-fit text-amber-800 text-center rounded-full text-xs md:text-sm px-4 py-1'>
+                Now in beta — free for early educators
+            </div>
+            <div className='font-dm-serif text-4xl lg:text-6xl my-4'>
+                <h1 className=''>Your courses.</h1>
+                <h1 className=''>Your students.</h1>
+                <h1 className='italic text-chart-3'>Your platform.</h1>
+            </div>
+            <div className='max-w-80 md:max-w-110 text-chart-3 balance leading-6'>
+                Edukate gives independent educators a complete digital teaching infrastructure with
+                course builder, student management, payments without a single line of code.
+            </div>
+            <div className='flex gap-4 my-6'>
+                <Link to="#get-started" className="bg-foreground text-background hover:bg-chart-3 text-sm font-medium py-3 px-6 rounded-sm transition duration-300">
+                    Start for free
+                </Link>
+                <Link to="#learn-more" className="bg-background text-foreground hover:text-chart-3 text-sm font-medium py-3 px-6 rounded-sm border border-chart-1 hover:border-chart-3 transition-all duration-300">
+                    See how it works
+                </Link>
+            </div>
+            <div>
+
+            </div>
+        </div>
+        <div className='hidden sm:flex justify-center items-center'>
+            <img src="course.png" width="500" alt="course-dashboard" />
+        </div>
+    </div>
+  )
+}
+
+
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import Hero from "../components/Landing/Hero";
 
 export default function Landing() {
   return (
     <>
-        <div className="max-w-6xl h-200 mx-auto">
-            <Navbar />
-            
+      <div>
+        <div className="max-w-6xl mx-auto">
+          <Navbar />
         </div>
+        <main className="max-w-6xl mx-auto">
+        <Hero />
+        </main>
+      </div>
     </>
-  )
+  );
 }
