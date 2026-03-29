caecff3e4b86947c43ef0947983099f2b7349677
Ojas Goyal <ojasgoyal05@gmail.com>
2026-03-29 17:07:41 +0530

    allow all cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,11 +17,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const corsOptions = {
-  origin: [allowedOrigin, /https:\/\/[a-zA-Z0-9-]+\.edukate\.in$/],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.options("/*splat", cors(corsOptions));
 
 app.use(express.json());
