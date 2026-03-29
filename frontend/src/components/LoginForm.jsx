df85471db1e4878dc9d09d1cfb19ed0b9d1f70b7
Ojas Goyal <ojasgoyal05@gmail.com>
2026-03-29 11:13:24 +0530

    courses

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import Navbar from "../components/navbar/Navbar";
 
 export default function Login() {
   const [isLogin, setIsLogin] = useState(true);
--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Courses() {
+  const { user } = useAuth();
+  const apiBaseUrl =
+    import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+
+  const [courses, setCourses] = useState([]);
+  const [loading , setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          `${apiBaseUrl}/api/courses/mycourses`,
+          {
+            withCredentials: true,
+          },
+        );
+        setCourses(response.data?.courses);
+        setLoading(false);
+        console.log("Fetched courses:", response.data?.courses);
+      } catch (error) {
+        setLoading(false);
+        console.error("Error fetching courses:", error);
+      }
+    };
+    
+    fetchCourses();
+  }, []);
+
   return (
-    <div>Courses</div>
-  )
+    <>
+      <div className="my-5 text-xl font-bold">Courses By Me</div>
+      <div className="">
+        {loading ? (
+          <p>Loading courses...</p>
+        ) : courses.length === 0 ? (
+          <p>You have not created any courses yet.</p>
+        ) : (
+          <div className="flex">
+            {courses.map((course) => (
+              <Link
+                className="w-50 h-30 bg-muted rounded-sm flex flex-col justify-end p-4 gap-2 mr-4"
+                key={course._id}
+                to={`/courses/${course.courseSlug}`}
+              >
+                <div className="font-medium">Course: {course.title}</div>
+                <div className="text-sm">Description: {course.description}</div>
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
