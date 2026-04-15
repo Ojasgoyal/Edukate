1d9164f53e232fa887668ff0de665e9bf3b228e5
Ojas Goyal <ojasgoyal05@gmail.com>
2026-04-15 17:30:42 +0530

    Course Padding

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -13,10 +13,11 @@ const fetcher = async (url) => {
 export default function Courses() {
   const { userData } = useAuth();
 
-  const { data: courses, error, isLoading } = useSWR(
-    `${apiBaseUrl}/api/courses/mycourses`,
-    fetcher
-  );
+  const {
+    data: courses,
+    error,
+    isLoading,
+  } = useSWR(`${apiBaseUrl}/api/courses/mycourses`, fetcher);
 
   return (
     <>
@@ -29,7 +30,7 @@ export default function Courses() {
               <div
                 key={n}
                 // Matches your card size, adds a pulse animation and a soft background
-                className="w-52 h-32 bg-muted/60 animate-pulse rounded-sm flex flex-col justify-end p-4 gap-2 mr-4"
+                className="w-64 h-48 bg-muted/60 animate-pulse rounded-sm flex flex-col justify-end p-4 gap-2 mr-4"
               >
                 {/* Fake Title Line */}
                 <div className="h-4 bg-muted-foreground/20 rounded w-2/3"></div>
@@ -46,15 +47,28 @@ export default function Courses() {
           <div className="flex gap-4 flex-wrap">
             {courses.map((course) => (
               <Link
-                className="w-fit h-fit bg-background/80 shadow border border-px border-chart-1 rounded-sm flex flex-col justify-center px-6 py-8 gap-2 transition-transform hover:scale-102"
+                className="w-fit h-fit bg-background/80 shadow border border-px border-chart-1 rounded-sm flex flex-col justify-start gap-2 transition-transform hover:scale-102"
                 key={course._id}
                 to={`/dashboard/courses/edit/${course.courseSlug}`}
               >
-                <div>
-                  <img className="w-64 object-cover rounded-sm " src={course.thumbnail} alt={course.title} />
+                <img
+                  className="w-64 object-cover rounded-t-sm"
+                  src={course.thumbnail}
+                  alt={course.title}
+                />
+
+                <div className="flex h-fit justify-between pt-1 pb-5 px-4">
+                  <div className="flex flex-col gap-2">
+                    <div className="text-xl font-semibold truncate">
+                      {course.title}
+                    </div>
+                    <div className="text-sm truncate line-clamp-2">
+                      {course.description}
+                    </div>
+                  </div>
+
+                  <div className="font-semibold text-md pr-2">₹{course.price}</div>
                 </div>
-                <div className="text-xl font-semibold truncate">{course.title}</div>
-                <div className="text-sm truncate line-clamp-2">{course.description}</div>
               </Link>
             ))}
           </div>
